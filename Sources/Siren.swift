1767043acf5e5c30300b991a9f30895d31ed83cf
tlacan <lacan.thomas@gmail.com>
2018-10-26 16:51:30 +0200

    Remove Final on class to inherit Siren
    Inherit Siren to do manual check with firebase

--- Sources/Siren.swift
+++ Sources/Siren.swift
@@ -11,7 +11,7 @@ import UIKit
 // MARK: - Siren
 
 /// The Siren Class. A singleton that is initialized using the `shared` constant.
-public final class Siren: NSObject {
+public class Siren: NSObject {
 
     /// Current installed version of your app.
     internal var currentInstalledVersion: String? = {
